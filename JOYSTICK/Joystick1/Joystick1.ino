1a15dc50c49eed448a80ff96e39704328f531bb9
Andres Rangel <andres.rangel@hulu.com>
2017-10-25 07:14:42 -0700

    Cleaned up to make it only the joystick code

--- JOYSTICK/Joystick1/Joystick1.ino
+++ JOYSTICK/Joystick1/Joystick1.ino
@@ -1,6 +1,6 @@
 
 #include "Joystick.h"
-#include "Radio.cpp"
+//#include "Radio.cpp"
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -8,8 +8,6 @@
 #define SWX A0
 #define SWY A1
 #define SWBUTTON  8
-#define CE_PIN   9
-#define CSN_PIN 10
 
 Joystick joystick(SWX, SWY, SWBUTTON);
 
@@ -20,29 +18,6 @@ void setup() {
   joystick.begin();
 }
 
-void send(int ) {
-
-    bool rslt;
-    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
-        // Always use sizeof() as it gives the size as the number of bytes.
-        // For example if dataToSend was an int sizeof() would correctly return 2
-
-    Serial.print("Data Sent ");
-    Serial.print(dataToSend);
-    if (rslt) {
-      if ( radio.isAckPayloadAvailable() ) {
-              radio.read(&ackData, sizeof(ackData));
-              newData = true;
-       }
-       else {
-            Serial.println("  Acknowledge but no data ");
-       }
-       updateMessage();
-    }
-    else {
-        Serial.println("  Tx failed");
-    }
-}
 
 
 
