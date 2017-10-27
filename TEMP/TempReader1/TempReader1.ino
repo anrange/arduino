8c547790353c15eedd5cc6e7ba0cb2fd40ae5d25
Andres Rangel <andres.rangel@hulu.com>
2017-10-27 08:54:55 -0700

    First iteration of the temp probe.

---
+++ TEMP/TempReader1/TempReader1.ino
@@ -0,0 +1,28 @@
+#include <max6675.h>
+/**
+ * Obtained from:
+ * http://henrysbench.capnfatz.com/henrys-bench/arduino-temperature-measurements/max6675-temp-module-arduino-manual-and-tutorial/
+ */
+int ktcSO = 8;
+int ktcCS = 9;
+int ktcCLK = 10;
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
+
+  
+void setup() {
+  Serial.begin(9600);
+  // give the MAX a little time to settle
+  delay(500);
+}
+
+void loop() {
+  // basic readout test
+  
+   Serial.print("Deg C = "); 
+   Serial.print(ktc.readCelsius());
+   Serial.print("\t Deg F = ");
+   Serial.println(ktc.readFahrenheit());
+ 
+   delay(500);
+}
