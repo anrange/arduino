627591bd929d58a12c92854395655695e39a4238
Arkin Yetis <arkin.yetis@hulu.com>
2018-08-01 12:57:14 -0700

    Initial checkin for ROBOT2.

---
+++ ROBOT2/Robot2_joystick_rx/UltraSound.h
@@ -0,0 +1,18 @@
+#ifndef UltraSound_h
+#define UltraSound_h
+
+#include "arduino.h"
+
+class UltraSound
+{
+  public:
+    UltraSound(int trigger,int echo);
+    void begin();
+    long readDistance();
+    boolean isThereObstacle();
+    int trigger,echo;
+    long OBSTACLE_LIMIT = 400;
+
+};
+#endif
+
